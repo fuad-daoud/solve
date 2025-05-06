c2ecf569b808f6ec7c63966978833d48a9ebf9fa
Fuad Daoud <fuad.daoud@icloud.com>
2025-05-06 09:55:38 +0300

    feat: solved A,B from Educational Codeforces Round 6

--- main.cpp
+++ main.cpp
@@ -1,57 +1,65 @@
-#include <cstdio>
-#include <iostream>
-#include <queue>
-#include <utility>
-#include <vector>
 
+#include <iostream>
 using namespace ::std;
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
 void solve() {
-  int n, m;
-  cin >> n >> m;
-  int k;
-  cin >> k;
+  int a, b;
+  cin >> a >> b;
+  int answer = 0;
+  while (a <= b) {
+    int tmp = a;
+    int mod = 10;
+    int div = 1;
 
-  queue<pair<int, int>> q;
-  vector<vector<bool>> vis(n, vector<bool>(m, false));
-
-  for(int i = 0; i < k; i++) {
-    int x, y;
-    cin >> x >> y;
-    x--;y--;
-    vis[x][y] = true;
-    q.push(make_pair(x, y));
-  }
-  int x = -1;
-  int y = -1;
-
-  while(!q.empty()) {
-    x = q.front().first;
-    y = q.front().second;
-    q.pop();
-
-    for(int i = 0; i < 4; i++) {
-      int nx = x + dx[i];
-      int ny = y + dy[i];
-      if(nx < 0 || ny < 0 || nx == n || ny == m || vis[nx][ny]) {
-	continue;
+    while (true) {
+      if (tmp / div == 0) {
+        break;
       }
-      vis[nx][ny] = true;
-      q.push(make_pair(nx, ny));
-    }
-  }
-  cout << x + 1 << ' ' << y + 1<< endl;
+      int current = (tmp % mod) / div;
+      mod *= 10;
+      div *= 10;
 
+      switch (current) {
+      case 0:
+      case 6:
+      case 9:
+        answer += 6;
+        break;
+      case 1:
+        answer += 2;
+        break;
+      case 2:
+      case 3:
+      case 5:
+        answer += 5;
+        break;
+      case 4:
+        answer += 4;
+        break;
+      case 7:
+        answer += 3;
+        break;
+      case 8:
+        answer += 7;
+        break;
+      default:
+        exit(123);
+      }
+    }
+    a++;
+
+  }
+  cout << answer << endl;
 }
 
 int main() {
-#ifdef ONLINE_JUDGE
-  freopen("output.txt", "w", stdout);
-  freopen("input.txt", "r", stdin);
-#endif
+  // #ifdef ONLINE_JUDGE
+  // freopen("output.txt", "w", stdout);
+  // freopen("input.txt", "r", stdin);
+  // #endif
   int cases = 1;
   /*cin >> cases;*/
   while (cases--) {
