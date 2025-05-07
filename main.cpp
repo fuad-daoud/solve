6d8f9d896ade417e2ae42ee5747c807d67902070
Fuad Daoud <fuad.daoud@icloud.com>
2025-05-07 17:44:42 +0300

    feat: solved C from Educational Codeforces Round 6

--- main.cpp
+++ main.cpp
@@ -1,64 +1,51 @@
-
+// lookuped up the tutorial, turned out I did not understand the problem
+#include <cassert>
 #include <iostream>
+#include <set>
+#include <vector>
 using namespace ::std;
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
 void solve() {
-  int a, b;
-  cin >> a >> b;
-  int answer = 0;
-  while (a <= b) {
-    int tmp = a;
-    int mod = 10;
-    int div = 1;
-
-    while (true) {
-      if (tmp / div == 0) {
+  int n;
+  cin >> n;
+  vector<vector<int>> answer;
+  for (int i = 0, j = 0; i < n; i = j) {
+    set<int> seen;
+    int x = -1;
+    cin >> x;
+    while (j < n && !seen.count(x)) {
+      seen.insert(x);
+      j++;
+      if(j == n) {
         break;
       }
-      int current = (tmp % mod) / div;
-      mod *= 10;
-      div *= 10;
-
-      switch (current) {
-      case 0:
-      case 6:
-      case 9:
-        answer += 6;
-        break;
-      case 1:
-        answer += 2;
-        break;
-      case 2:
-      case 3:
-      case 5:
-        answer += 5;
-        break;
-      case 4:
-        answer += 4;
-        break;
-      case 7:
-        answer += 3;
-        break;
-      case 8:
-        answer += 7;
-        break;
-      default:
-        exit(123);
-      }
+      cin >> x;
     }
-    a++;
-
+    if (j == n) {
+      break;
+    }
+    answer.push_back({i + 1, j + 1});
+    j++;
   }
-  cout << answer << endl;
+  if(answer.empty()) {
+    cout << -1 << endl;
+    return;
+  }
+  answer.back().back() = n;
+
+  cout << answer.size() << endl;
+  for(int i = 0; i < answer.size(); i++) {
+    cout << answer[i][0] << ' ' << answer[i][1] << endl;
+  }
 }
 
 int main() {
   // #ifdef ONLINE_JUDGE
+  // assert(freopen("input2.txt", "rt", stdin));
   // freopen("output.txt", "w", stdout);
-  // freopen("input.txt", "r", stdin);
   // #endif
   int cases = 1;
   /*cin >> cases;*/
