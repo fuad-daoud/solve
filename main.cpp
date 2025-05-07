202a20b9d46d5cf22bcef3c752b42dc611a25357
Fuad Daoud <fuad.daoud@icloud.com>
2025-05-07 18:19:17 +0300

    feat: solved A from Educational Codeforces Round 5

--- main.cpp
+++ main.cpp
@@ -1,44 +1,50 @@
-// lookuped up the tutorial, turned out I did not understand the problem
 #include <cassert>
+#include <cmath>
+#include <cstdlib>
 #include <iostream>
-#include <set>
-#include <vector>
+
 using namespace ::std;
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
 void solve() {
-  int n;
-  cin >> n;
-  vector<vector<int>> answer;
-  for (int i = 0, j = 0; i < n; i = j) {
-    set<int> seen;
-    int x = -1;
-    cin >> x;
-    while (j < n && !seen.count(x)) {
-      seen.insert(x);
-      j++;
-      if(j == n) {
-        break;
-      }
-      cin >> x;
-    }
-    if (j == n) {
-      break;
-    }
-    answer.push_back({i + 1, j + 1});
-    j++;
+  string a, b;
+  cin >> a >> b;
+  string tmp = "";
+  int diff = abs(int(a.size() - b.size()));
+  for (int i = 0; i < diff; i++) {
+    tmp.append("0");
   }
-  if(answer.empty()) {
-    cout << -1 << endl;
+  if (a.size() < b.size()) {
+    a.insert(0, tmp);
+  } else if (b.size() < a.size()) {
+    b.insert(0, tmp);
+  }
+  int i = 0, j = 0;
+
+  while (i < a.size() && j < b.size()) {
+    if (a[i] > b[j]) {
+      cout << ">" << endl;
+      return;
+    } else if (a[i] < b[j]) {
+      cout << "<" << endl;
+      return;
+    }
+    i++, j++;
+  }
+  if (i == a.size() && j == b.size()) {
+    cout << "=" << endl;
     return;
   }
-  answer.back().back() = n;
+  if (i == a.size()) {
+    cout << "<" << endl;
+    return;
+  }
 
-  cout << answer.size() << endl;
-  for(int i = 0; i < answer.size(); i++) {
-    cout << answer[i][0] << ' ' << answer[i][1] << endl;
+  if (j == b.size()) {
+    cout << "<" << endl;
+    return;
   }
 }
 
