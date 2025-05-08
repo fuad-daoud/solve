83f1af6fb3afd980ac6e4eb21312c73197d28563
Fuad Daoud <fuad.daoud@icloud.com>
2025-05-08 16:33:46 +0300

    feat: solved C from Educational Codeforces Round 5

--- main.cpp
+++ main.cpp
@@ -1,37 +1,102 @@
-#include <algorithm>
 #include <cassert>
-#include <cstdint>
-#include <vector>
 #include <iostream>
+#include <set>
 
 using namespace ::std;
 
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
+const int N = 1001, M = 1001;
+
+bool vis[N][M];
+pair<int, int> leader_map[N][M];
+int leaders[N][M];
+string grid[N];
+
+int check(int y, int x, int n, int m, int leader_y, int leader_x) {
+  if (y == -1 || y >= n) {
+    return 0;
+  }
+  if (x == -1 || x >= m) {
+    return 0;
+  }
+  if (vis[y][x]) {
+    return 0;
+  }
+  if (grid[y][x] == '*') {
+    return 0;
+  }
+  vis[y][x] = true;
+  leader_map[y][x] = {leader_y, leader_x};
+  int size = 1;
+
+  for (int i = 0; i < 4; i++) {
+    size += check(y + dy[i], x + dx[i], n, m, leader_y, leader_x);
+  }
+  return size;
+}
+
 void solve() {
   int n, m;
   cin >> n >> m;
-  vector<vector<int>> data(n, vector<int>(m));
-  for(int i = 0; i < n; i++) {
-    for(int j = 0; j < m; j++) {
-      cin >> data[i][j];
+
+  for (int i = 0; i < n; i++) {
+    string x;
+    cin >> x;
+    grid[i] = x;
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (vis[i][j]) {
+        continue;
+      }
+      leaders[i][j] = check(i, j, n, m, i, j);
     }
   }
-  for(int i = 0; i < n; i++) {
-    sort(data[i].begin(), data[i].end());
-  }
-  int answer = INT32_MIN;
 
-  for(int i = 0; i < n; i++) {
-    answer = max(answer, data[i].front());
+  string answer[n];
+  for (int i = 0; i < n; i++) {
+    answer[i] = grid[i];
   }
-  cout << answer << endl;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (grid[i][j] == '.') {
+        continue;
+      }
+      set<pair<int, int>> seen_leaders;
+      int size = 1;
+      for (int k = 0; k < 4; k++) {
+        int y = i + dy[k], x = j + dx[k];
+        if (y == -1 || y >= n) {
+          continue;
+        }
+        if (x == -1 || x >= m) {
+          continue;
+        }
+        if(grid[y][x] == '*') {
+          continue;
+        }
+        pair<int, int> leader = leader_map[y][x];
+        if (seen_leaders.count(leader)) {
+          continue;
+        }
+        size += leaders[leader.first][leader.second];
+        seen_leaders.insert(leader);
+      }
+      answer[i][j] = size % 10 + '0';
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    cout << answer[i] << endl;
+  }
 }
 
 int main() {
   // #ifdef ONLINE_JUDGE
-  // assert(freopen("input2.txt", "rt", stdin));
+  // assert(freopen("input.txt", "rt", stdin));
   // freopen("output.txt", "w", stdout);
   // #endif
   int cases = 1;
