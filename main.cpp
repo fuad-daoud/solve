8bc74a1edd4872578a39275e8c4c52e51c521e01
Fuad Daoud <fuad.daoud@icloud.com>
2025-05-09 19:28:41 +0300

    feat: solved A,B from Educational Codeforces Round 178

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
+#include <algorithm>
 #include <cassert>
 #include <iostream>
-#include <set>
 
 using namespace ::std;
 
@@ -9,98 +9,41 @@ int dy[4] = {0, 0, -1, 1};
 
 const int N = 1001, M = 1001;
 
-bool vis[N][M];
-pair<int, int> leader_map[N][M];
-int leaders[N][M];
-string grid[N];
-
-int check(int y, int x, int n, int m, int leader_y, int leader_x) {
-  if (y == -1 || y >= n) {
-    return 0;
-  }
-  if (x == -1 || x >= m) {
-    return 0;
-  }
-  if (vis[y][x]) {
-    return 0;
-  }
-  if (grid[y][x] == '*') {
-    return 0;
-  }
-  vis[y][x] = true;
-  leader_map[y][x] = {leader_y, leader_x};
-  int size = 1;
-
-  for (int i = 0; i < 4; i++) {
-    size += check(y + dy[i], x + dx[i], n, m, leader_y, leader_x);
-  }
-  return size;
-}
-
 void solve() {
-  int n, m;
-  cin >> n >> m;
+  int n;
+  cin >> n;
+  long long arr[n];
 
   for (int i = 0; i < n; i++) {
-    string x;
-    cin >> x;
-    grid[i] = x;
+    cin >> arr[i];
   }
 
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      if (vis[i][j]) {
-        continue;
-      }
-      leaders[i][j] = check(i, j, n, m, i, j);
-    }
+  long long cm[n + 1];
+  cm[n] = 0l;
+  for(int i = n - 1; i >= 0; i--) {
+    cm[i] = cm[i + 1] + arr[i];
   }
-
-  string answer[n];
-  for (int i = 0; i < n; i++) {
-    answer[i] = grid[i];
+  long long mx[n];
+  mx[0] = arr[0];
+  for(int i = 1; i < n; i++) {
+    mx[i] = max(mx[i - 1], arr[i]);
   }
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      if (grid[i][j] == '.') {
-        continue;
-      }
-      set<pair<int, int>> seen_leaders;
-      int size = 1;
-      for (int k = 0; k < 4; k++) {
-        int y = i + dy[k], x = j + dx[k];
-        if (y == -1 || y >= n) {
-          continue;
-        }
-        if (x == -1 || x >= m) {
-          continue;
-        }
-        if(grid[y][x] == '*') {
-          continue;
-        }
-        pair<int, int> leader = leader_map[y][x];
-        if (seen_leaders.count(leader)) {
-          continue;
-        }
-        size += leaders[leader.first][leader.second];
-        seen_leaders.insert(leader);
-      }
-      answer[i][j] = size % 10 + '0';
-    }
+  for (int i = 1; i <= n; i++) {
+    int last_index = n - i;
+    long long last = mx[last_index];
+    last += cm[last_index + 1];
+    cout << last << ' ';
   }
-
-  for (int i = 0; i < n; i++) {
-    cout << answer[i] << endl;
-  }
+  cout << endl;
 }
 
 int main() {
-  // #ifdef ONLINE_JUDGE
-  // assert(freopen("input.txt", "rt", stdin));
-  // freopen("output.txt", "w", stdout);
-  // #endif
+#ifndef ONLINE_JUDGE
+  assert(freopen("input.txt", "rt", stdin));
+// freopen("output.txt", "w", stdout);
+#endif
   int cases = 1;
-  /*cin >> cases;*/
+  cin >> cases;
   while (cases--) {
     solve();
   }
