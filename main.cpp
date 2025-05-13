0bfa427048e72fad5badca5057c1a48de3fc56a8
Fuad Daoud <fuad.daoud@icloud.com>
2025-05-13 17:04:37 +0000

    feat: solved Reading Books CSES, geeksforgeeks solution

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <cassert>
 #include <cstdio>
 #include <iostream>
-#include <queue>
+#include <numeric>
 
 using namespace ::std;
 
@@ -14,90 +14,20 @@ const int N = 1001, M = 1001;
 void solve() {
   int n;
   cin >> n;
-  pair<long long , int> arr[n];
-  queue<pair<long long, int>> k_books;
-  queue<pair<long long , int>> j_books;
+  long long arr[n];
   for (int i = 0; i < n; i++) {
-    cin >> arr[i].first;
-    arr[i].second = i;
+    cin >> arr[i];
   }
   sort(arr, arr + n);
-  for (int i = 0; i < n; i++) {
-    k_books.push(arr[i]);
-  }
-  reverse(arr, arr + n);
-  for (int i = 0; i < n; i++) {
-    j_books.push(arr[i]);
-  }
 
-  long long answer = 0;
-  int locked[n];
+  long long mx = arr[n - 1];
 
-  while (!k_books.empty() && !j_books.empty()) {
-
-    if (k_books.front().second == j_books.front().second) {
-      // TODO: create a heap for both to solve such cases
-      if (locked[k_books.front().second] == 1) {
-        pair<int, int> tmp = j_books.front();
-        j_books.pop();
-        j_books.push(tmp);
-      } else {
-        pair<int, int> tmp = k_books.front();
-        k_books.pop();
-        k_books.push(tmp);
-      }
-      if (k_books.front().second == j_books.front().second) {
-        if (locked[k_books.front().second] == 1) {
-          answer += k_books.front().first;
-          locked[k_books.front().second] = 0;
-          k_books.pop();
-          if (!k_books.empty() && !locked[k_books.front().second]) {
-            locked[k_books.front().second] = 1;
-          }
-        } else {
-          answer += j_books.front().first;
-          locked[j_books.front().second] = 0;
-          j_books.pop();
-          if (!j_books.empty() && !locked[j_books.front().second]) {
-            locked[j_books.front().second] = 2;
-          }
-        }
-      }
-      continue;
-    }
-
-    if (k_books.front().first < j_books.front().first) {
-      answer += k_books.front().first;
-      j_books.front().first -= k_books.front().first;
-      locked[k_books.front().second] = 0;
-      k_books.pop();
-      if (!k_books.empty() && !locked[k_books.front().second]) {
-        locked[k_books.front().second] = 1;
-      }
-    } else if (k_books.front().first > j_books.front().first) {
-      answer += j_books.front().first;
-      k_books.front().first -= j_books.front().first;
-      locked[j_books.front().second] = 0;
-      j_books.pop();
-      if (!j_books.empty() && !locked[j_books.front().second]) {
-        locked[j_books.front().second] = 2;
-      }
-    } else {
-      answer += j_books.front().first;
-      j_books.pop();
-      k_books.pop();
-    }
+  long long sm = accumulate(arr, arr + n - 1, 0ll);
+  if (mx <= sm) {
+    cout << sm + mx << endl;
+    return;
   }
-
-  while (!k_books.empty()) {
-    answer += k_books.front().first;
-    k_books.pop();
-  }
-  while (!j_books.empty()) {
-    answer += j_books.front().first;
-    j_books.pop();
-  }
-  cout << answer << endl;
+  cout << mx * 2 << endl;
 }
 
 int main() {
