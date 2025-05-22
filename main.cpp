803752e4d34bc9c2debbde97a84127adc24f4ce4
Fuad Daoud <fuad.daoud@icloud.com>
2025-05-22 16:50:48 +0300

    feat: dynamic programming baby solved grid paths

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <cstdio>
+#include <cstring>
 #include <iostream>
 
 using namespace ::std;
@@ -7,13 +8,17 @@ using namespace ::std;
 int dx[4] = {0, 0, 1, -1}; // Up, Down, Right, Left
 int dy[4] = {-1, 1, 0, 0}; // Up, Down, Right, Left
 
-const int N = 1000, M = 48;
+const int N = 1000, M = 48, mod = 1e9 + 7;
 #define in(i, j) i >= 0 && i < j
 #define not_in(i, j) !(in(i, j))
 int n;
 string grid[N];
+int dp[N][N];
 
 int go(int row = 0, int column = 0) {
+  if (dp[row][column] != -1) {
+    return dp[row][column];
+  }
   if (grid[row][column] == '*') {
     return 0;
   }
@@ -23,21 +28,23 @@ int go(int row = 0, int column = 0) {
   int result = 0;
 
   if (in(row + 1, n) && in(column, n)) {
-    result += go(row + 1, column);
+    result = (result + go(row + 1, column)) % mod;
   }
-
   if (in(row, n) && in(column + 1, n)) {
-    result += go(row, column + 1);
+    result = (result + go(row, column + 1))% mod;
   }
+  dp[row][column] = result % mod;
   return result;
 }
 
 void solve() {
   cin >> n;
+  memset(dp, -1, sizeof(dp));
   for (int i = 0; i < n; i++) {
     cin >> grid[i];
   }
-  cout << go() << endl;;
+  dp[0][0] = go();
+  cout << dp[0][0] << endl;
 }
 
 int main() {
