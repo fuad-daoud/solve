cfbdeef89a0c10dc060897769bd84da716ba9a11
Fuad Daoud <fuad.daoud@icloud.com>
2025-10-25 12:47:22 +0000

    feat: solved B from Round 290 (Div.2) (graph)

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <limits.h>
 #include <map>
 #include <numeric>
+#include <set>
 #include <stack>
 #include <stdio.h>
 
@@ -36,54 +37,72 @@ int dp[N][500];
 bool visited[N][500];
 
 
-void solve() {
-    int m, t, r;
-    cin >> m >> t >> r;
-    int after_midnight[601] = {};
-    int before_midnight[601] = {};
-    int ghosts[m];
-    for (int i = 0; i < m; i++) {
-        cin >> ghosts[i];
-    }
-    for (int ghost: ghosts) {
-        int count = 0;
-        for (int i = ghost - t; i < ghost; i++) {
-            if (i < 0) {
-                count += before_midnight[i * -1];
-                continue;
-            }
-            count += after_midnight[i];
-        }
-        for (int i = ghost - 1; i >= ghost - t; i--) {
-            if (count >= r) {
-                break;
-            }
-            if (i < 0) {
-                if (before_midnight[i * -1] == 0) {
-                    before_midnight[i * -1]++;
-                    count++;
-                }
-                continue;
-            }
-            if (after_midnight[i] == 0) {
-                count++;
-                after_midnight[i]++;
-            }
-        }
-        if (count < r) {
-            cout << -1 << endl;
-            return;
-        }
-    }
-    int answer = 0;
+struct parent_node {
+    int r;
+    int c;
+};
 
-    for (const int i: before_midnight) {
-        answer += i;
+struct node {
+    parent_node parent;
+    int r;
+    int c;
+};
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    string grid[n];
+    for (int i = 0; i < n; i++) {
+        cin >> grid[i];
     }
-    for (const int i: after_midnight) {
-        answer += i;
+    vector<pair<int, int> > graph[n][m];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            char color = grid[i][j];
+            if (j + 1 < m && color == grid[i][j + 1]) {
+                graph[i][j].emplace_back(i, j + 1);
+                graph[i][j + 1].emplace_back(i, j);
+            }
+            if (i + 1 < n && color == grid[i + 1][j]) {
+                graph[i][j].emplace_back(i + 1, j);
+                graph[i + 1][j].emplace_back(i, j);
+            }
+        }
     }
-    cout << answer << endl;
+    bool vis[n][m];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            vis[i][j] = false;
+        }
+    }
+    stack<node> s;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < m - 1; j++) {
+            if (vis[i][j]) {
+                continue;
+            }
+            s.emplace(node{parent_node{-1, -1}, i, j});
+            while (!s.empty()) {
+                auto [parent, r, c] = s.top();
+                if (vis[r][c]) {
+                    cout << "Yes";
+                    return;
+                }
+                if (parent.r == r && parent.c == c) {
+                    continue;
+                }
+                vis[r][c] = true;
+                s.pop();
+                for (const auto [row, column]: graph[r][c]) {
+                    if (parent.r == row && parent.c == column) {
+                        continue;
+                    }
+                    s.push(node{parent_node{r, c}, row, column});
+                }
+            }
+        }
+    }
+    cout << "No" << endl;
 }
 
 
