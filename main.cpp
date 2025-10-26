edea500bc9f9ee8b95a9af7b9c99e17195eace55
Fuad Daoud <fuad.daoud@icloud.com>
2025-10-26 06:03:15 +0000

    feat: solved A from Round 291 (Div.2) (graph)

--- main.cpp
+++ main.cpp
@@ -49,60 +49,19 @@ struct node {
 };
 
 void solve() {
-    int n, m;
-    cin >> n >> m;
-    string grid[n];
-    for (int i = 0; i < n; i++) {
-        cin >> grid[i];
-    }
-    vector<pair<int, int> > graph[n][m];
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            char color = grid[i][j];
-            if (j + 1 < m && color == grid[i][j + 1]) {
-                graph[i][j].emplace_back(i, j + 1);
-                graph[i][j + 1].emplace_back(i, j);
-            }
-            if (i + 1 < n && color == grid[i + 1][j]) {
-                graph[i][j].emplace_back(i + 1, j);
-                graph[i + 1][j].emplace_back(i, j);
-            }
+    string s;
+    cin >> s;
+    bool pass_first = false;
+    for (const char c: s) {
+        if (c == '9' && !pass_first) {
+            cout << c;
+        } else if ((c - '0') >= 5) {
+            cout << 9 - (c - '0');
+        } else {
+            cout << c;
         }
+            pass_first = true;
     }
-    bool vis[n][m];
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            vis[i][j] = false;
-        }
-    }
-    stack<node> s;
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < m - 1; j++) {
-            if (vis[i][j]) {
-                continue;
-            }
-            s.emplace(node{parent_node{-1, -1}, i, j});
-            while (!s.empty()) {
-                auto [parent, r, c] = s.top();
-                if (vis[r][c]) {
-                    cout << "Yes";
-                    return;
-                }
-                if (parent.r == r && parent.c == c) {
-                    continue;
-                }
-                vis[r][c] = true;
-                s.pop();
-                for (const auto [row, column]: graph[r][c]) {
-                    if (parent.r == row && parent.c == column) {
-                        continue;
-                    }
-                    s.push(node{parent_node{r, c}, row, column});
-                }
-            }
-        }
-    }
-    cout << "No" << endl;
 }
 
 
