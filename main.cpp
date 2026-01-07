f8a546f952473e57c72572340e25efc68a57af24
Fuad Daoud <fuad.daoud@icloud.com>
2026-01-07 13:25:16 +0300

    feat: solved C from Round 485 (Div.2) doing ladder on cfladders.rf.gd

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <limits.h>
 #include <map>
 #include <numeric>
+#include <queue>
 #include <set>
 #include <stack>
 #include <stdio.h>
@@ -34,75 +35,41 @@ int dy[4] = {-1, 1, 0, 0}; // Up, Down, Right, Left
 void solve() {
     int n;
     cin >> n;
-    int a;
-    cin >> a;
-    deque<int> arr_a;
-    for (int i = 0; i < a; i++) {
-        int x;
-        cin >> x;
-        arr_a.push_front(x);
+    int sizes[n], costs[n];
+    for (int i = 0; i < n; i++) {
+        cin >> sizes[i];
     }
-    int b;
-    cin >> b;
-    deque<int> arr_b;
-    for (int i = 0; i < b; i++) {
-        int x;
-        cin >> x;
-        arr_b.push_front(x);
+
+    for (int i = 0; i < n; i++) {
+        cin >> costs[i];
     }
-    vector<deque<int> > state_a, state_b;
-    state_a.emplace_back(arr_a);
-    state_b.emplace_back(arr_b);
-    int rounds = 0;
-    while (!arr_a.empty() && !arr_b.empty()) {
-        rounds++;
-        if (arr_b.back() > arr_a.back()) {
-            int smaller_card = arr_a.back();
-            arr_a.pop_back();
-            arr_b.push_front(smaller_card);
-            int bigger_card = arr_b.back();
-            arr_b.pop_back();
-            arr_b.push_front(bigger_card);
-        } else {
-            int smaller_card = arr_b.back();
-            arr_b.pop_back();
-            arr_a.push_front(smaller_card);
-            int bigger_card = arr_a.back();
-            arr_a.pop_back();
-            arr_a.push_front(bigger_card);
-        }
 
+    long long mins[n];
 
-        for (int i = 0; i < state_a.size(); i++) {
-            bool same = arr_a.size() == state_a[i].size() && arr_b.size() == state_b[i].size();
-            if (!same) {
-                continue;
-            }
-
-            for (auto it = arr_a.begin(), it_c = state_a[i].begin(); it != arr_a.end() && it_c != state_a[i].end();
-                 ++it, ++it_c) {
-                if (*it != *it_c) {
-                    same = false;
-                    break;
-                }
-            }
-
-            for (auto it = arr_b.begin(), it_c = state_b[i].begin(); it != arr_b.end() && it_c != state_b[i].end();
-                 ++it, ++it_c) {
-                if (*it != *it_c) {
-                    same = false;
-                    break;
-                }
-            }
-            if (same) {
-                cout << -1 << endl;
-                return;
+    for (int i = n - 2; i >= 0; i--) {
+        const int current = sizes[i];
+        int mn = INT_MAX;
+        for (int j = i + 1; j < n; j++) {
+            if (current < sizes[j]) {
+                mn = min(mn, costs[j]);
             }
         }
-        state_a.emplace_back(arr_a);
-        state_b.emplace_back(arr_b);
+        mins[i] = mn;
     }
-    cout << rounds << ' ' << (arr_a.empty() ? 2 : 1) << endl;
+    long long answer = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        const int current = sizes[i];
+        for (int j = i + 1; j < n; j++) {
+            if (current < sizes[j]) {
+                answer = min(answer, costs[i] + costs[j] + mins[j]);
+            }
+        }
+    }
+    if (answer == INT_MAX) {
+        cout << -1 << endl;
+        return;
+    }
+    cout << answer << endl;
 }
 
 
