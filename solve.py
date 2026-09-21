21bd4ac3eae7ac0db8b5f68fc5e157b4d97fed21
Fuad Daoud <fuad.daoud@icloud.com>
2026-09-21 18:23:10 +0300

    feat: solved 42. Trapping Rain Water [Hard]

--- solve.py
+++ solve.py
@@ -4,32 +4,19 @@
 
 class Solution:
     def trap(self, height: list[int]) -> int:
-        current = 0
-        current_area = 0
-        prev = 0
-        answer = 0
-        while current != len(height):
-            if prev <= height[current]:
-                answer += current_area
-                prev = height[current]
-                current_area = 0
+        l, r = 0, len(height) - 1
+        left_max = right_max = 0
+        water = 0
+        while l < r:
+            if height[l] < height[r]:
+                left_max = max(left_max, height[l])
+                water += left_max - height[l]
+                l += 1
             else:
-                current_area += prev - height[current]
-            current += 1
-
-        current = 0
-        current_area = 0
-        prev = 0
-        height = list(reversed(height))
-        while current != len(height):
-            if prev < height[current]:
-                answer += current_area
-                prev = height[current]
-                current_area = 0
-            else:
-                current_area += prev - height[current]
-            current += 1
-        return answer
+                right_max = max(right_max, height[r])
+                water += right_max - height[r]
+                r -= 1
+        return water
 
 
 if __name__ == "__main__":
